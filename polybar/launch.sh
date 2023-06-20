cde161dcdb58cc127c19cc44e6214f36eeba135c
CamiloArias12 <ariaspira13@gmail.com>
2023-06-20 12:46:31 -0500

    config-linux

---
+++ polybar/launch.sh
@@ -0,0 +1,8 @@
+
+polybar --config=~/myconfig/polybar/polybarConfig.ini date &
+polybar --config=~/myconfig/polybar/polybarConfig.ini battery &
+polybar --config=~/myconfig/polybar/polybarConfig.ini power &
+polybar --config=~/myconfig/polybar/polybarConfig.ini xworkspaces &
+polybar --config=~/myconfig/polybar/polybarConfig.ini xwindow &
+polybar --config=~/myconfig/polybar/polybarConfig.ini logo &
+polybar --config=~/myconfig/polybar/polybarConfig.ini desc &
